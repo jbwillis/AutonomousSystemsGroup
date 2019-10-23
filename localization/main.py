e77571def32555b48a86aa84f4be6bc0d0849fc5
Jacob Willis <jbwillis272@gmail.com>
2019-10-23 13:13:12 -0600

    interpolate odometry to line up with measurements

--- localization/main.py
+++ localization/main.py
@@ -21,42 +21,37 @@ if __name__ == "__main__":
     phi0 = params.theta0
     mu = np.array([x0, y0, phi0])
     Sigma = np.eye(3)
+    
+    v_interp = np.interp(np.squeeze(l_time), np.squeeze(odom_t), np.squeeze(vel_odom[0,:]))
+    w_interp = np.interp(np.squeeze(l_time), np.squeeze(odom_t), np.squeeze(vel_odom[1,:]))
 
     t_prev = 0.0
-    meas_index = 0
-    for i in range(odom_t.size):
+    for meas_index in range(l_time.size):
         #stuff for plotting
         mu_hist.append(mu)
-        dt = odom_t.item(i) - t_prev
-        t_prev = odom_t.item(i)
-
-        #find which measurements to use
-        while l_time.item(meas_index) < odom_t.item(i):
-            meas_index += 1
-            if meas_index >= l_time.size:
-                break
-        meas_index -= 1
+        dt = l_time.item(meas_index) - t_prev
+        t_prev = l_time.item(meas_index)
         
         lm_ind = np.argwhere(np.isfinite(l_depth[:,meas_index]))
         r = l_depth[lm_ind, meas_index]
         phi = l_bearing[lm_ind, meas_index]
         zt = np.hstack((r, phi)).T
 
-        mu, Sigma, K = ukf.update(mu, Sigma, zt, lm_ind, vel_odom[0,i], vel_odom[1,i], dt)
+        mu, Sigma, K = ukf.update(mu, Sigma, zt, lm_ind, v_interp[meas_index], w_interp[meas_index], dt)
 
     fig1, ax1 = plt.subplots(nrows=3, ncols=1, sharex=True)
     mu_hist = np.array(mu_hist).T
     odom_t = odom_t.flatten()
     ax1[0].plot(t_truth, x_truth, label="Truth")
-    ax1[0].plot(odom_t, mu_hist[0,:], label="Est")
+    ax1[0].plot(l_time, mu_hist[0,:], label="Est")
     ax1[0].set_ylabel("x (m)")
     ax1[0].legend()
     ax1[1].plot(t_truth, y_truth, label="Truth")
-    ax1[1].plot(odom_t, mu_hist[1,:], label="Est")
+    ax1[1].plot(l_time, mu_hist[1,:], label="Est")
     ax1[1].set_ylabel("y (m)")
     ax1[1].legend()
     ax1[2].plot(t_truth, th_truth, label="Truth")
-    ax1[2].plot(odom_t, mu_hist[2,:], label="Est")
+    ax1[2].plot(l_time, mu_hist[2,:], label="Est")
     ax1[2].set_xlabel("Time (s)")
     ax1[2].set_ylabel("$\psi$ (rad)")
     ax1[2].legend()
